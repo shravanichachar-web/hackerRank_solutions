de589fede66354c53d96485ca685fcd3397eaff1
Shravani Chachar <shravanichachar@gmail.com>
2026-08-06 17:46:50 +0530

    Add integer and float division functionality

---
+++ division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Print the result of integer division
+    print(a // b)
+    
+    # Print the result of float division
+    print(a / b)
