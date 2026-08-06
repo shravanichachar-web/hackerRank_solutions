d60322fec194356c75e6874b5baadc3c23044cde
Shravani Chachar <shravanichachar@gmail.com>
2026-08-06 17:01:16 +0530

    Implement if-else and  check with conditions

---
+++ If_else.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    # If n is odd, print Weird
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        # If n is even and in the inclusive range of 2 to 5
+        if 2 <= n <= 5:
+            print("Not Weird")
+        # If n is even and in the inclusive range of 6 to 20
+        elif 6 <= n <= 20:
+            print("Weird")
+        # If n is even and greater than 20
+        elif n > 20:
+            print("Not Weird")
