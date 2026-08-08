6863124164403df19ed0778bc31589e03fee4128
Shravani Chachar <shravanichachar@gmail.com>
2026-08-08 13:36:54 +0530

    Implement string manipulation in Create_strings.cpp

---
+++ Create_strings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    
+    cin >> a >> b;
+    
+    cout << a.size() << " " << b.size() << "\n";
+    
+    cout << a + b << "\n";
+    
+    
+    char temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+    
+    cout << a << " " << b << "\n";
+    
+    return 0;
+	
+  
+    return 0;
+}
