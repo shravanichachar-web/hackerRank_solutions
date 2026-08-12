c7b7ae75a1786b0879a11ca0b76ced94830209fa
Shravani Chachar <shravanichachar@gmail.com>
2026-08-12 19:15:12 +0530

    Implement exception handling for division and input errors

---
+++ Create_Exceptions.py
@@ -0,0 +1,23 @@
+def main():
+    
+    t = int(input())
+    
+    for _ in range(t):
+        try:
+            a, b = input().split()
+            
+            print(int(a) // int(b))
+        except ZeroDivisionError as e:
+            
+            print("Error Code:", e)
+            
+        except ValueError as e:
+            
+            print("Error Code:", e)
+            
+            
+if __name__  == '__main__':
+    main()
+
+
+
