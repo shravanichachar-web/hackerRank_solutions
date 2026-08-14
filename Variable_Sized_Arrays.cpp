8404b7e65cc3329b2dadbcdbe415808fcb08385b
Shravani Chachar <shravanichachar@gmail.com>
2026-08-14 18:09:08 +0530

    Add Variable Sized Arrays implementation
    Implement variable sized arrays and query functionality.

---
+++ Variable_Sized_Arrays.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n, q;
+    
+    cin >> n >> q;
+    
+    vector<vector<int>> a(n);
+    
+    for (int i = 0; i < n; i++) {
+        int k;
+        cin >> k;
+        
+        a[i].resize(k);
+        
+        for (int j = 0; j < k; j++) {
+            cin >> a[i][j];
+        }
+    }
+    
+    for (int q_num = 0; q_num < q; q_num++) {
+        int i, j;
+        cin >> i >> j;
+        
+        cout << a[i][j] << endl;
+    }
+    
+  
+    return 0;
+}
