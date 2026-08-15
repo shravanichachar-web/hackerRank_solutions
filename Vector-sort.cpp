f8eb1b1bf853193e89ab739f530f7f264abadfe8
Shravani Chachar <shravanichachar@gmail.com>
2026-08-15 11:44:41 +0530

    Add vector sorting functionality in Vector-sort.cpp
    Implement a program to sort a vector of integers.

---
+++ Vector-sort.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> v;
+    
+    for(int i = 0; i < n; i++){
+        int val;
+        cin >> val;
+        v.push_back(val);
+    }
+    
+    sort(v.begin(), v.end());
+    
+    for(int i = 0; i < n; i++){
+        cout << v[i] << " ";
+    }
+    
+    return 0;
+}
